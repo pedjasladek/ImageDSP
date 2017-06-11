92dba06c5ed2e91f7a2702a0d8e7a1fd29435ffb
Stefan <niketic95@gmail.com>
2017-06-11 02:22:46 +0200

    Added colour to rotation

--- src/ImageInterpolation.cpp
+++ src/ImageInterpolation.cpp
@@ -74,12 +74,10 @@ void bilinearInterpolate(const uchar input[], int xSize, int ySize, uchar output
                     + a*(1-b)*yOld[bl*xSize +  ah] + a*b*yOld[bh*xSize + ah];
             if (!(j%2) && !(i%2))
             {
-                {
-                    uNew[j / 2 * newXSize / 2 + i / 2] = (1-a)*(1-b)*uOld[bl / 2 * xSize / 2 + al / 2] + (1-a)*b*uOld[bh / 2 * xSize / 2 + al / 2]
-                            + a*(1-b)*uOld[bl / 2 * xSize / 2 +  ah / 2] + a*b*uOld[bh / 2 * xSize / 2 + ah / 2];
-                    vNew[j / 2 * newXSize / 2 + i / 2] = (1-a)*(1-b)*vOld[bl / 2 * xSize / 2 + al / 2] + (1-a)*b*vOld[bh / 2 * xSize / 2 + al / 2]
-                            + a*(1-b)*vOld[bl / 2 * xSize / 2 +  ah / 2] + a*b*vOld[bh / 2 * xSize / 2 + ah / 2];
-                }
+                uNew[j / 2 * newXSize / 2 + i / 2] = (1-a)*(1-b)*uOld[bl / 2 * xSize / 2 + al / 2] + (1-a)*b*uOld[bh / 2 * xSize / 2 + al / 2]
+                    + a*(1-b)*uOld[bl / 2 * xSize / 2 +  ah / 2] + a*b*uOld[bh / 2 * xSize / 2 + ah / 2];
+                vNew[j / 2 * newXSize / 2 + i / 2] = (1-a)*(1-b)*vOld[bl / 2 * xSize / 2 + al / 2] + (1-a)*b*vOld[bh / 2 * xSize / 2 + al / 2]
+                    + a*(1-b)*vOld[bl / 2 * xSize / 2 +  ah / 2] + a*b*vOld[bh / 2 * xSize / 2 + ah / 2];
             }
         }
     }
@@ -119,9 +117,19 @@ void imageRotate(const uchar input[], int xSize, int ySize, uchar output[], int 
             if(xCoord < xSize && yCoord < ySize && xCoord >= 0 && yCoord >= 0)
             {
                 yNew[j*xSize+i] = yOld[yCoord*xSize+xCoord];
+                if (!(j%2) && !(i%2))
+                {
+                    uNew[j/2*xSize/2+i/2] = uOld[yCoord/2*xSize/2+xCoord/2];
+                    vNew[j/2*xSize/2+i/2] = vOld[yCoord/2*xSize/2+xCoord/2];
+                }
             }else
             {
                 yNew[j*xSize+i] = 0;
+                if (!(j%2) && !(i%2))
+                {
+                    uNew[j/2*xSize/2+i/2] = 0;
+                    vNew[j/2*xSize/2+i/2] = 0;
+                }
             }
         }
     }
